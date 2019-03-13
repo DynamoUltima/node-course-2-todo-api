8a98f6024f9a0d9125318962cc2ebf759ab3d17f
Joel Dynamo <dynamo.joey@gmail.com>
2019-03-13 08:57:11 +0000

    /POST route

--- server/server.js
+++ server/server.js
@@ -23,16 +23,18 @@ app.post('/todos', (req, res) => {
     //     res.status(400).send(e);
     // })
 
-    var anotherTodo= new Todo({
-        text:req.body.text
+    var anotherTodo = new Todo({
+        text: req.body.text
     })
-    anotherTodo.save().then((doc)=>{
+    anotherTodo.save().then((doc) => {
         res.send(doc)
-    },(e)=>{
+    }, (e) => {
         res.status(400).send(e)
     })
-    
+
 })
 app.listen(3000, () => {
     console.log('Started on port 3000')
 })
+
+module.exports = {app};
