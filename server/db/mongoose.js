cdfab578b6768b4d75bd87f04ecf17428a4417af
Joel Dynamo <dynamo.joey@gmail.com>
2019-03-16 12:19:57 +0000

    mongoDB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+//mongoose.Promise = global.Promise;
 
 //work around future deprecation >  mongoose.connect("mongodb://localhost:27017/YourDB", { useNewUrlParser: true });
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
