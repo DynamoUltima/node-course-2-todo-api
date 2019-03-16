867583dec2aac1ecf370ce0da359956281bdadbd
Joel Dynamo <dynamo.joey@gmail.com>
2019-03-16 11:24:27 +0000

    database connected

--- server/server.js
+++ server/server.js
@@ -2,10 +2,14 @@ var express = require('express')
 var bodyParser = require('body-parser')
 
 
-var { mongoose } = require('./db/mongoose.js')
+var { mongoose } = require('./db/mongoose')
 
 var { Todo } = require('./models/todos');
 var { User } = require('./models/user');
+const { ObjectID } = require('mongoDB')
+
+
+
 
 var app = express();
 app.use(bodyParser.json())
@@ -23,6 +27,8 @@ app.post('/todos', (req, res) => {
     //     res.status(400).send(e);
     // })
 
+
+
     var anotherTodo = new Todo({
         text: req.body.text
     })
@@ -33,8 +39,34 @@ app.post('/todos', (req, res) => {
     })
 
 })
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    })
+}, (e) => {
+    res.status(400).send(e)
+})
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+
+    }
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    })
+
+})
+
 app.listen(3000, () => {
     console.log('Started on port 3000')
 })
 
-module.exports = {app};
+module.exports = { app };
