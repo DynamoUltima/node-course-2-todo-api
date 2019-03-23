0c02f1b21168ad69c6d7f4075121fdfe1ae85f1f
Joel Dynamo <dynamo.joey@gmail.com>
2019-03-23 22:57:44 +0000

    Delete route added

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,20 @@
+const { mongoose } = require('../server/db/mongoose')
+const { Todo } = require('../server/models/todos')
+const { ObjectID } = require('mongoDB')
+const { User } = require('../server/models/user')
+
+
+// Todo.remove({}).then((result)=>{
+//     console.log(result)
+// })
+// Todo.deleteMany({}).then((result) => {
+//     console.log(result)
+// })
+
+// future findOneAndUpdate, findOneAndReplace or findOneAndDelete instead
+
+Todo.findOneAndRemove({ _id:'5c965eac81a4c319b4206703'}).then(())
+
+Todo.findByIdAndRemove('5c965eac81a4c319b4206703').then((todo)=>{
+    console.log(todo);
+})
